c3033e803e26e9f6527d72892cdcb04bde5745f2
mexzzy <samuelmeshach055@gmail.com>
2023-08-30 19:34:18 +0100

    changes on toggle

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const toggle = document.getElementById("toggle");
 const navbar = document.getElementById("navbar");
-
+const openIcon = document.getElementById("openIcon")
+const closeIcon = document.getElementById("closeIcon")
 toggle.addEventListener("click", function () {
   toggleCheck();
 });
@@ -15,10 +16,14 @@ function toggleCheck() {
 function open() {
   isToggle = false;
   navbar.style.display = "flex";
+  closeIcon.style.display = "block"
+  openIcon.style.display = "none"
 }
 function close() {
   isToggle = true;
   navbar.style.display = "none";
+  closeIcon.style.display = "none"
+  openIcon.style.display = "block"
 }
 document.querySelector(".infoWrapper").addEventListener("click", () => {
   close();
